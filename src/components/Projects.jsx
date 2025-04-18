762b5fb928ede338e45cdd0aef310851ac59fba2
DESKTOP-ERCLAHV\Leo <117135186+dicksonLoo@users.noreply.github.com>
2025-04-18 15:51:59 +0800

    update 18/04/2025
    -updated scrollbar

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -42,7 +42,7 @@ const Projects = () => {
               {project.technologies.map((tech, index) => (
                 <span
                   key={index}
-                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
                 >
                   {tech}
                 </span>
